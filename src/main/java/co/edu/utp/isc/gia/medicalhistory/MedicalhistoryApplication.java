095b61351762ba4adcecae72ca11ef5c187b0ac3
SantiagoOcampoOrrego <santiagoocampoorrego@gmail.com>
2021-12-28 13:29:26 -0500

    Initial commit

---
+++ src/main/java/co/edu/utp/isc/gia/medicalhistory/MedicalhistoryApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.medicalhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalhistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalhistoryApplication.class, args);
+	}
+
+}
